37f437195131a344a72d11ca77e0513b88e248da
student <abdullahihamdiabass@gmail.com>
2021-05-06 12:14:02 +0300

    first commit

---
+++ src/today.kt
@@ -0,0 +1,48 @@
+fun main(){
+product()
+    divide()
+    modulus()
+}
+fun introduction(name,age){
+var name="John"
+    var age=26
+   println(My name is $name and I am $age years old)
+
+
+}
+
+fun names(){
+    val names=Arrayof("Hamdi","whato","tumainini")
+   return(array.len>4)
+
+}
+fun product() {
+    var num1 = 30
+    var num2 = 40
+    var product=num1*num2
+    println(product)
+
+}
+fun divide(){
+    var num1 = 30
+    var num2 = 40
+    var divide=num1/num2
+    println(divide)
+}
+fun modulus(){
+    var num1 = 30
+    var num2 = 40
+    var modulus=num1%num2
+    println(modulus)
+}
+fun comparison(){
+
+}
+fun jina(){
+    arrayOf("Hamdi","Aisha","jaki","ann","mama")
+    println(names)
+}
+class Human(name,age,weight){
+
+
+}
